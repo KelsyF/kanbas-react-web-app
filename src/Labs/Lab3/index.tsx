b933b96e6d6f3d6288db8955edfd3f295010bc1b
KelsyF <frank.ke@northeastern.edu>
2024-10-11 18:49:35 -0400

    Started A3 - Lab 3 up to ImpliedReturn

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,30 @@
+
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInLine from "./ConditionalOutputInLine";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+
 export default function Lab3() {
     return (
         <div>
-            <h2>Lab 3</h2>
+            <h2>Lab 3 - Javascript</h2>
+            <VariablesAndConstants/>
+            <VariableTypes/>
+            <BooleanVariables/>
+            <IfElse/>
+            <TernaryOperator/>
+            <ConditionalOutputIfElse/>
+            <ConditionalOutputInLine/>
+            <hr/>
+            <LegacyFunctions/>
+            <ArrowFunctions/>
+            <ImpliedReturn/>
         </div>
     );
 }
